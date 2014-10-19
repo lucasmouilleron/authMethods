b01d58da27341cffef14ae2dfb6aab9942766b1f
Lucas Mouilleron <lucas.mouilleron@me.com>
2014-10-19 14:32:20 +0200

    metas

--- _build/Gruntfile.js
+++ _build/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         options: {
           sort: true,
           filter: "include",
-          tasks: ["default","intro","cleanup","speed","compile:images","watch","build","watch:scripts", "compile:scripts", "compile:styles", "watch:styles", "sync"]
+          tasks: ["default","cleanup","speed","compile:images","watch","build","watch:scripts", "compile:scripts", "compile:styles", "watch:styles"]
         }
       }
     },
@@ -37,6 +37,7 @@ module.exports = function(grunt) {
           outputStyle:"compressed",
           relativeAssets:true,
           lineComments:false,
+          force:true,
           raw: "preferred_syntax = :sass\n",
           environment: "production"
         }
@@ -54,52 +55,6 @@ module.exports = function(grunt) {
       everything: {
         files: ["<%=cfg.sassDir%>/**/*.scss", "<%=cfg.jsDir%>/**/*.js"],
         tasks: ["compile:scripts", "compile:styles"]
-      }
-    },
-    browserSync: {
-      dev: {
-        bsFiles: {
-          src : ["<%=cfg.cssDir%>/**/*.css", "<%=webDir%>/**/*.php", "<%=webDir%>/**/*.html", "<%=webDir%>/**/*.js"]
-        },
-        options: {
-          host: "<%=cfg.host%>",
-          proxy: "http://<%=cfg.host%>/<%=cfg.baseURL%>/"
-        }
-      }
-    },
-    svgmin: {
-      default: {
-        files: [{
-          expand: true,
-          cwd: "<%=cfg.imgSrcDir%>",
-          src: ['**/*.svg'],
-          dest: "<%=cfg.imgDir%>"
-        }]
-      }
-    },
-    imagemin: {
-      default: {
-        files: [{
-          expand: true,
-          cwd: "<%=cfg.imgSrcDir%>",
-          src: ["**/*.{png,jpg,gif}"],
-          dest: "<%=cfg.imgDir%>"
-        }]
-      }
-    },
-    grunticon: {
-      default: {
-        files: [{
-          expand: true,
-          cwd:"<%=cfg.iconsDir%>",
-          src: ['*.svg', '*.png'],
-          dest: "<%=cfg.imgDir%>/icons"
-        }],
-        options: {
-          datasvgcss: "icons.css",
-          datapngcss: "icons.png.css",
-          previewhtml: "icons.preview.html"
-        }
       }
     },
     clean: {
@@ -130,10 +85,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-requirejs");
   grunt.loadNpmTasks("grunt-contrib-compass");
-  grunt.loadNpmTasks("grunt-browser-sync");
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
-  grunt.loadNpmTasks("grunt-svgmin");
-  grunt.loadNpmTasks("grunt-grunticon");
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-autoprefixer");
 
@@ -146,6 +97,4 @@ module.exports = function(grunt) {
   grunt.registerTask("compile:scripts", "Compile js files",["requirejs:compile"]);
   grunt.registerTask("compile:styles", "Watch and compile sass files",["compass:compile","autoprefixer"]);
   grunt.registerTask("build", "Build all (scripts + styles)",["compile:styles","compile:scripts", "cleanup"]);
-  grunt.registerTask("compile:images", "Optimize images and icons",["imagemin:default", "svgmin:default", "grunticon:default"]);
-  grunt.registerTask("sync", "Sync browser",["browserSync:dev"]);
 };
